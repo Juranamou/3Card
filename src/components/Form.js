4453292e339a121b00007da6e344e8d5cd8054e0
John Economou <johnlafayeti@gmail.com>
2022-09-12 21:07:53 -0400

    using react app for the first time, made a very simple form, and setup useState hook

---
+++ src/components/Form.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+export default function Form() {
+    const [input, setInput] = useState(0);
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(input);
+    }
+
+    return (
+    <div className='form-wrapper'>
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <input type="text" onChange={(e) => {setInput(e.target.value)}}></input>
+            <input type="submit"></input>
+        </form>
+    </div>
+    )
+}
